6d458394b569b9992046b667fb3fbdd37c3231b6
UDAYVEERUS <udayveerus348567@gmail.com>
2024-06-04 00:21:37 +0530

    Main file

---
+++ index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const express = require('express');
+
+const connectToMongo = require('./common/db');
+connectToMongo();
+
+const app = express();
+app.use(express.json());
+
+const port = 3022;
+
+app.get("/" , (req, res) => {
+    res.send("Hello World");
+});
+
+app.use("/user" , require('./user/route'))
+
+app.listen(`${port}` , ()  => {
+    console.log(`Server is listening on localhost:${port}`);
+})
